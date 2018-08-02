b631384faf02c569bb9d758ad75cc0604a69f84b
Ahmed Abdallah Nasr <AhmedAbdallahNasr@outlook.com>
2018-08-02 16:47:29 +0200

    v3

--- Configuration/Constants.js
+++ Configuration/Constants.js
@@ -14,5 +14,6 @@ var Commands = {};
 Commands.GET_STARTED = "CMD_523189_GETSTARTED";
 Commands.NOT_NOW="CMD_85326_NOTNOW";
 Commands.TTW3 = "CMD_23143_TTW3";
+Commands.LANG = "CMD_23674_LANGUAGE:"
 
 module.exports = { ActionTypes: ActionTypes, WebViewRatio: WebViewRatio,Commands:Commands };
--- routes/Facebook.js
+++ routes/Facebook.js
@@ -4,6 +4,7 @@ var request = require('request');
 var sleep = require('thread-sleep');
 var configurations = require('../Configuration/Config');
 var CONSTANTS = require('../Configuration/Constants');
+var fs = require('fs');
 
 /* Facebook Auth. */
 router.get('/api/Facebook/', function (req, res) {
@@ -13,19 +14,19 @@ router.get('/api/Facebook/', function (req, res) {
         res.send('Error, wrong token')
     }
 });
-router.get('/api/Init',function(req,res){
+router.get('/api/Init', function (req, res) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
         qs: { access_token: configurations.PageAccessToken },
         method: 'POST',
-        json: { 
-            "get_started":{
-              "payload":CONSTANTS.Commands.GET_STARTED
+        json: {
+            "get_started": {
+                "payload": CONSTANTS.Commands.GET_STARTED
             }
-          }
-    }, function (error, response, body) {   
+        }
+    }, function (error, response, body) {
         console.log(body);
-             
+
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -37,34 +38,34 @@ router.get('/api/Init',function(req,res){
         qs: { access_token: configurations.PageAccessToken },
         method: 'POST',
         json: {
-            "persistent_menu":[
-              {
-                "locale":"default",
-                "composer_input_disabled": true,
-                "call_to_actions":[
-                  {
-                        "type":"postback",
-                        "title":"تطوع",
-                        "payload":CONSTANTS.Commands.TTW3
-                      }
+            "persistent_menu": [
+                {
+                    "locale": "default",
+                    "composer_input_disabled": true,
+                    "call_to_actions": [
+                        {
+                            "type": "postback",
+                            "title": "تطوع",
+                            "payload": CONSTANTS.Commands.TTW3
+                        }
                     ]
-                  },{
-                    "locale":"en_us",
-                    "composer_input_disabled":false,
-                    "call_to_actions":[
-                      {
-                        "type":"postback",
-                        "title":"Volunteer",
-                        "payload":CONSTANTS.Commands.TTW3
-                      }
-                    ]    
-                  }
-              
-                ]
-              }
-    }, function (error, response, body) {   
+                }, {
+                    "locale": "en_us",
+                    "composer_input_disabled": false,
+                    "call_to_actions": [
+                        {
+                            "type": "postback",
+                            "title": "Volunteer",
+                            "payload": CONSTANTS.Commands.TTW3
+                        }
+                    ]
+                }
+
+            ]
+        }
+    }, function (error, response, body) {
         console.log(body);
-             
+
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -73,6 +74,53 @@ router.get('/api/Init',function(req,res){
     });
     res.status(200).end();
 });
+function GetUser(clientMessengerID,callback) {
+    let read = fs.readFile('Users.txt', 'utf8', function (err, data) {
+        var jsonObj = JSON.parse(data);
+        jsonObj.forEach(element => {
+            if (element.id === clientMessengerID)
+                callback(element);
+        });
+    });
+}
+function SaveUser(user) {
+    let read = fs.readFile('Users.txt', 'utf8', function (err, data) {
+        var jsonObj = JSON.parse(data);
+        let isExist = false;
+        jsonObj.forEach(element => {
+            if (element.id === user.id)
+                isExist = true;
+        });
+        if (!isExist) {
+            jsonObj.push(user);
+        }
+        var writeStream = fs.createWriteStream("Users.txt");
+        var jsonContent = JSON.stringify(jsonObj);
+        writeStream.write(jsonContent);
+        writeStream.close();
+
+
+    });
+
+}
+function GetFacebookUserData(clientMessengerID, callback) {
+    request({
+        url: 'https://graph.facebook.com/' + clientMessengerID,
+        qs: {
+            fields: "first_name,last_name,profile_pic",
+            access_token: configurations.PageAccessToken
+        },
+        method: 'GET'
+    }, function (error, response, body) {
+        callback(body)
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    });
+
+}
 router.post('/api/Facebook/', function (req, res) {
     var Activity = {};
     let messaging_info = req.body.entry[0].messaging;
@@ -82,9 +130,17 @@ router.post('/api/Facebook/', function (req, res) {
             let event = messaging_info[i];
             //Get The User ID (Client Messenger ID).
             let senderID = event.sender.id;
+            GetFacebookUserData(senderID, function (params) {
+                var jsonObj = JSON.parse(params);
+                SaveUser(jsonObj);  
+            });
+            GetUser(senderID, function (params) {
+                
+            });
+            
             //Check If the User Sent a Message.
-            if(event.message && event.message.quick_reply){
-                HandlePostbackEvent(senderID,event.message.quick_reply.payload);
+            if (event.message && event.message.quick_reply) {
+                HandlePostbackEvent(senderID, event.message.quick_reply.payload);
             }
             else if (event.message && event.message.text) {
                 //Handle User Text Message and Response To the User.
@@ -105,38 +161,82 @@ router.post('/api/Facebook/', function (req, res) {
 
 function HandlePostbackEvent(SenderID, Postback) {
     if (Postback === CONSTANTS.Commands.GET_STARTED) {
-        SendDynamicTextMessage(SenderID, "اهلاً بك يا {{first_name}} فى الــ Hajj Eye"); 
-        sleep(500);
+        SendDynamicTextMessage(SenderID, "اهلاً بك يا {{first_name}} فى الــ Hajj Eye");
+        sleep(1000);
         let quickReplyActions = [];
-        quickReplyActions.push(QuickReplyAction('لاحقاً',CONSTANTS.Commands.NOT_NOW));
-        quickReplyActions.push(QuickReplyAction('تطوع',CONSTANTS.Commands.TTW3));  
-        
-        let quickReply = QuickReply("يمكنك التطوع لمساعدة اشخاص خلال فترة الحج، و تربح نقاط تشترى بها من علامات تجارية",quickReplyActions,SenderID);
+        quickReplyActions.push(QuickReplyAction('اتحدث العربية',CONSTANTS.Commands.LANG+'ar'));
+        quickReplyActions.push(QuickReplyAction('Speak English',CONSTANTS.Commands.LANG+'en'));
+
+        let quickReply = QuickReply("اخبرنى ما اللغة التى تتحدث بها", quickReplyActions, SenderID);
         SendQuickReply(quickReply)
-    }else if (Postback === CONSTANTS.Commands.TTW3) {
-        SendDynamicTextMessage(SenderID,"رائع يا {{first_name}}.\n سوف ابلغك وقت وجود شخص يحتاج الى مساعدة فى محادثة فيديو.");
-    }else if (Postback === CONSTANTS.Commands.NOT_NOW) {
-        SendTextMessage(SenderID,"حسناً يمكنك الانضمام الينا فى اى وقت.");
+    } else if (Postback === CONSTANTS.Commands.TTW3) {
+        SendDynamicTextMessage(SenderID, "رائع يا {{first_name}}.\n سوف ابلغك وقت وجود شخص يحتاج الى مساعدة فى محادثة فيديو.");
+    } else if (Postback === CONSTANTS.Commands.NOT_NOW) {
+        SendTextMessage(SenderID, "حسناً يمكنك الانضمام الينا فى اى وقت.");
+    }else if (Postback.startsWith(CONSTANTS.Commands.LANG)) {
+        UpdateUserLang(SenderID,Postback.split(':')[1]);
+        let quickReplyActions = [];
+        quickReplyActions.push(QuickReplyAction('لاحقاً', CONSTANTS.Commands.NOT_NOW));
+        quickReplyActions.push(QuickReplyAction('تطوع', CONSTANTS.Commands.TTW3));
+
+        let quickReply = QuickReply("يمكنك التطوع لمساعدة اشخاص خلال فترة الحج، و تربح نقاط تشترى بها من علامات تجارية", quickReplyActions, SenderID);
+        SendQuickReply(quickReply)    
     }
+    
+}
+function ButtonsCard(title,actions,clientMessengerID) {
+    return {
+        "recipient":{
+          "id":clientMessengerID
+        },
+        "message":{
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"button",
+              "text":title,
+              "buttons": PrepareActionsData(actions)
+            }
+          }
+        }
+      }
 }
 
-function QuickReplyAction(title,payload){
-return {
-    "content_type":"text",
-    "title":title,
-    "payload":payload
-  }
+function UpdateUserLang(clientMessengerID,lang) {
+    let read = fs.readFile('Users.txt', 'utf8', function (err, data) {
+        var jsonObj = JSON.parse(data);
+        let user = {};
+        jsonObj.forEach(element => {
+            if (element.id === clientMessengerID)
+                element.lang = lang;
+        });
+
+        var writeStream = fs.createWriteStream("Users.txt");
+        var jsonContent = JSON.stringify(jsonObj);
+        writeStream.write(jsonContent);
+        writeStream.close();
+
+
+    });
+
 }
-function QuickReply(text,actions,senderID){
-return {
-    "recipient":{
-      "id":senderID
-    },
-    "message":{
-      "text": text,
-      "quick_replies":actions
+function QuickReplyAction(title, payload) {
+    return {
+        "content_type": "text",
+        "title": title,
+        "payload": payload
     }
-  }
+}
+function QuickReply(text, actions, senderID) {
+    return {
+        "recipient": {
+            "id": senderID
+        },
+        "message": {
+            "text": text,
+            "quick_replies": actions
+        }
+    }
 }
 function HandleTextMessage(SenderID, Message) {
     //Send The User Message.
